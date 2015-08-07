749613ba5a4dbaa4698bae2a5d100f1329ff2378
Annie <anniehoogendoorn@gmail.com>
2015-08-07 13:45:30 -0700

    add contact.php and app.php

--- app/app.php
+++ app/app.php
@@ -2,21 +2,69 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Contact.php";
 
+    session_start();
+
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        $test_contact = new Contact("Learn PHP.");
-        $another_test_contact = new Contact("Learn Drupal.");
-        $third_contact = new Contact("Visit France.");
 
-        $list_of_contacts = array($test_contact, $another_test_contact, $third_contact);
+    $app->get("/", function() {
+
+
         $output = "";
 
-        foreach ($list_of_contacts as $contact) {
-            $output = $output . "<p>" . $contact->getName() . "</p>";
+        $all_contacts = Contact::getAll();
+
+        if (!empty($all_contacts)) {
+            $output .= "
+                <h1>To Do List</h1>
+                <p>Here are all your tasks:</p>
+                ";
+
+            foreach ($all_contacts as $contact) {
+                $output .= "<p>" . $contact->getName() . "</p>";
+            }
         }
 
+        $output .= "
+        <form action='/contacts' method='post'>
+            <label for='name'>Enter Name</label>
+            <input id='name' name='name' type='text'>
+
+            <button type='submit'>Enter Name</button>
+        </form>
+        ";
+
+        $output .= "
+        <form action='/delete_tasks' method='post'>
+            <button type='submit'>Delete</button>
+        </form>
+        ";
+
         return $output;
+    });
+
+    $app->post("/contacts", function() {
+        $contact = new Contact($_POST['name']);
+        $contact->save();
+        return "
+            <h1>You created a contact!</h1>
+            <p>" . $contact->getName() . "</p>
+            <p><a href='/'>View your list of Contacts.</a></p>
+        ";
+    });
+
+    $app->post("/delete_contacts", function() {
+
+        Contact::deleteAll();
+
+        return "
+            <h1>List Cleared!</h1>
+            <p><a href='/'>Home</a></p>
+        ";
     });
 
     return $app;
--- src/Contact.php
+++ src/Contact.php
@@ -2,21 +2,39 @@
     class Contact
     {
         private $name;
-    }
 
-    function __construct($name)
-    {
-        $this->name = $name
-    }
 
-    function setName($new_name)
-    {
-        $this->name = (string) $new_name;
-    }
+        function __construct($name)
+        {
+            $this->name = $name;
+        }
 
-    function getName()
-    {
-        return $this->name;
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_contacts'] = array();
+        }
+
+
     }
 
 ?>
