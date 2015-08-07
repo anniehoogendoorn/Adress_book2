2d32438e2c04c9d2e2cab1b985702be17b68c0eb
Annie <anniehoogendoorn@gmail.com>
2015-08-07 14:25:11 -0700

    add contacts.html.twig file.

--- app/app.php
+++ app/app.php
@@ -10,62 +10,31 @@
 
     $app = new Silex\Application();
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array (
+        'twig.path' => __DIR__.'/../views'
+    ));
 
-    $app->get("/", function() {
 
+    $app->get("/", function() use ($app) {
 
-        $output = "";
-
-        $all_contacts = Contact::getAll();
-
-        if (!empty($all_contacts)) {
-            $output .= "
-                <h1>To Do List</h1>
-                <p>Here are all your tasks:</p>
-                ";
-
-            foreach ($all_contacts as $contact) {
-                $output .= "<p>" . $contact->getName() . "</p>";
-            }
-        }
-
-        $output .= "
-        <form action='/contacts' method='post'>
-            <label for='name'>Enter Name</label>
-            <input id='name' name='name' type='text'>
-
-            <button type='submit'>Enter Name</button>
-        </form>
-        ";
-
-        $output .= "
-        <form action='/delete_tasks' method='post'>
-            <button type='submit'>Delete</button>
-        </form>
-        ";
-
-        return $output;
+        return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
 
-    $app->post("/contacts", function() {
+    $app->post("/contacts", function() use ($app) {
         $contact = new Contact($_POST['name']);
         $contact->save();
-        return "
-            <h1>You created a contact!</h1>
-            <p>" . $contact->getName() . "</p>
-            <p><a href='/'>View your list of Contacts.</a></p>
-        ";
+        return $app['twig']->render('create_contact.html.twig', array('newtask' => $task));
     });
 
-    $app->post("/delete_contacts", function() {
-
-        Contact::deleteAll();
-
-        return "
-            <h1>List Cleared!</h1>
-            <p><a href='/'>Home</a></p>
-        ";
-    });
+    // $app->post("/delete_contacts", function() {
+    //
+    //     Contact::deleteAll();
+    //
+    //     return "
+    //         <h1>List Cleared!</h1>
+    //         <p><a href='/'>Home</a></p>
+    //     ";
+    // });
 
     return $app;
 
